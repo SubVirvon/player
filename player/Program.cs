3e747bfd7d0c97c2b7a76ef1238f1fdf2a24669d
Virvon <dan130205@gmail.com>
2022-10-25 13:06:46 +0300

    player

---
+++ player/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(100, 200);
+
+            player.ShowStats();
+        }
+    }
+
+    class Player
+    {
+        private int _speed;
+        private int _health;
+
+        public Player(int speed, int health)
+        {
+            _speed = speed;
+            _health = health;
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"скорость: {_speed}\nздоровье: {_health}");
+        }
+    }
+}
